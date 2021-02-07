05f7a0408e038eeba069f7c719d03c14f0f19847
TBExtent <jedpreist@gmail.com>
2021-02-07 17:09:49 +0000

    deformation from collisions

--- Assets/Scripts/Vehicles/Deformation/Squishing.cs
+++ Assets/Scripts/Vehicles/Deformation/Squishing.cs
@@ -114,8 +114,20 @@ public class Squishing : MonoBehaviour
         meshCollider.sharedMesh = mesh;
     }
 
+    //  This breaks if this is on a kinematic object (big sad)
+    private void OnCollisionEnter(Collision collision) {
+        Vector3 collisionForce = collision.impulse;
+        collisionForce /= Time.fixedDeltaTime;
+        collisionForce /= 200;
+
+        if (collisionForce.sqrMagnitude >= 0.01f) CollideMesh(collision.collider, collisionForce, true);
+    }
+
     public void CollideMesh(Collider collider, Vector3 collisionForce, bool addNoise) {
         List<VertexGroup> moved = new List<VertexGroup>();
+
+        //  Make a queue (it breadth first traversal time)
+        Queue<VertexGroup> vertexQueue = new Queue<VertexGroup>();
 
         for (int i = 0; i < meshGraph.groups.Count; i++) {
             VertexGroup current = meshGraph.groups[i];
@@ -124,13 +136,69 @@ public class Squishing : MonoBehaviour
             if (collider.ClosestPoint(vertex) == vertex) {
                 Vector3 deformation = collisionForce;
                 deformation /= vertexWeight;
+
+                if (deformation.sqrMagnitude > 0.25f) {
+                    deformation.Normalize();
+                    deformation *= 0.5f;
+                }
                 if (addNoise) deformation *= Random.value * 0.2f + 0.9f;
 
-                //  Maybe clamp magnitude?
-
-                current.MoveBy(vertices, deformation);
+                current.MoveBy(vertices, deformation, false);
                 moved.Add(current);
+                vertexQueue.Enqueue(current);
             }
         }
+
+        // Move each vertex, making sure that it doesn't stretch too far from its neighbours
+        while (vertexQueue.Count > 0) {
+            VertexGroup current = vertexQueue.Dequeue();
+
+            List<float> oldEdgeSqrLengths = new List<float>();
+            for (int j = 0; j < current.connectingEdges.Count; j++) {
+                oldEdgeSqrLengths.Add(current.connectingEdges[j].sqrLength);
+            }
+
+            for (int j = 0; j < current.connectingEdges.Count; j++) {
+                VertexGroup adjacent = current.connectingEdges[j].OtherVertexGroup(current);
+
+                //  Check if adjacent vertex has been moved.
+                if (moved.Contains(adjacent)) {
+                    //  Get vector of edge between vertices.
+                    Vector3 edge = current.pos - adjacent.pos;
+                    //  ohno edge too long
+                    if (edge.sqrMagnitude > stretchiness * stretchiness * oldEdgeSqrLengths[j]) {
+                        //  make edge right length
+                        edge.Normalize();
+                        float randomNoise = 1; 
+                        if (addNoise) randomNoise = Random.value * 0.2f + 0.9f;
+                        float edgeStretchiness = stretchiness * randomNoise;
+                        edge *= edgeStretchiness * Mathf.Sqrt(oldEdgeSqrLengths[j]);
+
+                        //  move vertices so edge is not too long.
+                        current.MoveTo(vertices, adjacent.pos + edge, false);
+                        current.connectingEdges[j].UpdateEdgeLength();
+                    }
+                }
+            }
+
+            moved.Add(current);
+
+            //  Add adjacent, unmoved vertices into the queue for traversal
+            for (int j = 0; j < current.connectingEdges.Count; j++) {
+                //  Get adjacent vertex group
+                VertexGroup adjacent = current.connectingEdges[j].OtherVertexGroup(current);
+
+                //  Add it to the queue if it hasn't already been moved
+                if (!vertexQueue.Contains(adjacent) && !moved.Contains(adjacent)) {
+                    vertexQueue.Enqueue(adjacent);
+                }
+            }
+        }
+
+        //  Update the mesh
+        mesh.vertices = vertices.ToArray();
+        mesh.RecalculateNormals();
+
+        meshCollider.sharedMesh = mesh;
     }
 }
