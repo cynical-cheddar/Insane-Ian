2ee59e97adc73bae7cc8d3c410430111f5f83316
Matt <matthew.crees@ntlworld.com>
2021-02-15 15:34:59 +0000

    Ugly sorting function, scoreboard fully functional now

--- Assets/Scripts/UI/ScoreboardBehaviour.cs
+++ Assets/Scripts/UI/ScoreboardBehaviour.cs
@@ -35,18 +35,58 @@ public class ScoreboardBehaviour : MonoBehaviour
         UpdateScores();
     }
 
+    int CalcScore(GamestateTracker.TeamDetails team) {
+        return team.kills * killValue + team.deaths * deathValue + team.assists * assistValue;
+    }
+
+    List<GamestateTracker.TeamDetails> SortTeams(List<GamestateTracker.TeamDetails> unsortedTeams) {
+        // Sort the list, not a stable sort
+        List<GamestateTracker.TeamDetails> sortedTeams = unsortedTeams;
+        sortedTeams.Sort((t1, t2) => CalcScore(t1).CompareTo(CalcScore(t2)));
+        sortedTeams.Reverse();
+
+        // Make sure teams with the same score are sorted
+        List<List<GamestateTracker.TeamDetails>> tempList = new List<List<GamestateTracker.TeamDetails>>();
+        for (int i = 0; i < sortedTeams.Count; i++) tempList.Add(new List<GamestateTracker.TeamDetails>());
+        int score = CalcScore(sortedTeams[0]);
+        int counter = 0;
+        tempList[0].Add(sortedTeams[0]);
+        // Split teams into groups of same score
+        for (int i = 1; i < sortedTeams.Count; i++) {
+            if (score == CalcScore(sortedTeams[i])) {
+                tempList[counter].Add(sortedTeams[i]);
+            } else {
+                counter++;
+                score = CalcScore(sortedTeams[i]);
+                tempList[counter].Add(sortedTeams[i]);
+            }
+        }
+        // Sort groups by teamId
+        for (int i = 0; i < tempList.Count; i++) {
+            tempList[i].Sort((t1, t2) => t1.teamId.CompareTo(t2.teamId));
+        }
+
+        // Add the now fully sorted teams to a list to return
+        List<GamestateTracker.TeamDetails> returnList = new List<GamestateTracker.TeamDetails>();
+        foreach (List<GamestateTracker.TeamDetails> teamGroup in tempList) {
+            foreach (GamestateTracker.TeamDetails team in teamGroup) {
+                returnList.Add(team);
+            }
+        }
+
+        return returnList;
+    }
+
     public void UpdateScores() {
         // Sort teams by score
-        List<GamestateTracker.TeamDetails> SortedTeams = gamestateTracker.schema.teamsList;
-        SortedTeams.Sort((t1, t2) => (t1.kills * killValue + t1.deaths * deathValue + t1.assists * assistValue).CompareTo(t2.kills * killValue + t2.deaths * deathValue + t2.assists * assistValue));
-        SortedTeams.Reverse();
+        List<GamestateTracker.TeamDetails> sortedTeams = SortTeams(gamestateTracker.schema.teamsList);
+        
         if (scoreboardIsExpanded) {
             // Display teams in order
-            for (int i = 0; i < SortedTeams.Count; i++) {
-                int score = SortedTeams[i].kills * killValue + SortedTeams[i].deaths * deathValue + SortedTeams[i].assists * assistValue;
-                teamPanels[i].TeamName.text = $"Team {SortedTeams[i].teamId}";
-                teamPanels[i].TeamScore.text = $"Score: {score}";
-                teamPanels[i].TeamKDA.text = $"K/D/A: {SortedTeams[i].kills}/{SortedTeams[i].deaths}/{SortedTeams[i].assists}";
+            for (int i = 0; i < sortedTeams.Count; i++) {
+                teamPanels[i].TeamName.text = $"Team {sortedTeams[i].teamId}";
+                teamPanels[i].TeamScore.text = $"Score: {CalcScore(sortedTeams[i])}";
+                teamPanels[i].TeamKDA.text = $"K/D/A: {sortedTeams[i].kills}/{sortedTeams[i].deaths}/{sortedTeams[i].assists}";
             }
             teamPanels[0].Position.sprite = positionImages[0];
             teamPanels[0].PositionShadow.sprite = positionImages[0];
@@ -54,12 +94,11 @@ public class ScoreboardBehaviour : MonoBehaviour
             // Display player's team score
             int teamId = gamestateTracker.getPlayerDetails(PhotonNetwork.LocalPlayer.ActorNumber).teamId;
             GamestateTracker.TeamDetails team = gamestateTracker.getTeamDetails(teamId);
-            int score = team.kills * killValue + team.deaths * deathValue + team.assists * assistValue;
             teamPanels[0].TeamName.text = $"Team {teamId}";
-            teamPanels[0].TeamScore.text = $"Score: {score}";
+            teamPanels[0].TeamScore.text = $"Score: {CalcScore(team)}";
             teamPanels[0].TeamKDA.text = $"K/D/A: {team.kills}/{team.deaths}/{team.assists}";
-            for (int i = 0; i < SortedTeams.Count; i++) {
-                if (SortedTeams[i].teamId == teamId) {
+            for (int i = 0; i < sortedTeams.Count; i++) {
+                if (sortedTeams[i].teamId == teamId) {
                     teamPanels[0].Position.sprite = positionImages[i];
                     teamPanels[0].PositionShadow.sprite = positionImages[i];
                 }
