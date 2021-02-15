f86d9c8a6d637f9289fa62a463c425fdcb93cf53
Matt <matthew.crees@ntlworld.com>
2021-02-15 12:07:10 +0000

    Fixed scoreboard from network rework

--- Assets/Scripts/UI/ScoreboardBehaviour.cs
+++ Assets/Scripts/UI/ScoreboardBehaviour.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,25 @@ using UnityEngine.UI;
 
 public class ScoreboardBehaviour : MonoBehaviour
 {
-    public Text scoreboardText;
+    public List<TeamPanelBehaviour> teamPanels;
     GamestateTracker gamestateTracker;
     public int killValue, deathValue, assistValue;
 
     // Start is called before the first frame update
     void Start() {
         gamestateTracker = FindObjectOfType<GamestateTracker>();
+        
         // Wait for the host to finish loading first
         Invoke("updateScores", 0.1f);
     }
 
     public void updateScores() {
-        string scoreboardDetails = "";
-        foreach (GamestateTracker.TeamDetails team in gamestateTracker.schema.teamsList) {
-            int score = team.kills * killValue + team.deaths * deathValue + team.assists * assistValue;
-            scoreboardDetails += $"Team {team.teamId} -- Score: {score} -- K/D/A: {team.kills}/{team.deaths}/{team.assists} \n";
-        }
-        scoreboardText.text = scoreboardDetails;
+        int teamId = gamestateTracker.getPlayerDetails(PhotonNetwork.LocalPlayer.ActorNumber).teamId;
+        GamestateTracker.TeamDetails team = gamestateTracker.getTeamDetails(teamId);
+        int score = team.kills * killValue + team.deaths * deathValue + team.assists * assistValue;
+        teamPanels[0].TeamName.text = $"Team {teamId}";
+        teamPanels[0].TeamScore.text = $"Score: {score}";
+        teamPanels[0].TeamKDA.text = $"K/D/A: {team.kills}/{team.deaths}/{team.assists}";
     }
 
 }
