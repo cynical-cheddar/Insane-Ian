6dab06e9dd6f84fe2c26eaa6552e1e49db4d9748
Matt <matthew.crees@ntlworld.com>
2021-02-16 14:14:35 +0000

    MVP ending screen completed

--- Assets/Scripts/UI/Scoring/PlinthManager.cs
+++ Assets/Scripts/UI/Scoring/PlinthManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PlinthManager : MonoBehaviour
 {
     public List<TextMesh> plinthTexts;
+    public TextMesh scoreboardText;
     GamestateTracker gamestateTracker;
     ScoringHelper scoringHelper = new ScoringHelper();
 
@@ -13,14 +14,20 @@ public class PlinthManager : MonoBehaviour
     void Start() {
         gamestateTracker = FindObjectOfType<GamestateTracker>();
         // Wait for the host to finish loading first
-        Invoke(nameof(UpdatePlinths), 0.1f);
+        Invoke(nameof(UpdateText), 0.1f);
     }
 
-    void UpdatePlinths() {
+    void UpdateText() {
 
         List<GamestateTracker.TeamDetails> sortedTeams = scoringHelper.SortTeams(gamestateTracker.schema.teamsList);
         plinthTexts[0].text = $"Team {sortedTeams[0].teamId}";
         if (sortedTeams.Count > 1) plinthTexts[1].text = $"Team {sortedTeams[1].teamId}";
         if (sortedTeams.Count > 2) plinthTexts[2].text = $"Team {sortedTeams[2].teamId}";
+
+        string newText = "";
+        foreach (GamestateTracker.TeamDetails team in sortedTeams) {
+            newText += $"Team {team.teamId} -- Score: {scoringHelper.CalcScore(team)} -- K/D/A: {team.kills}/{team.deaths}/{team.assists}\n";
+        }
+        scoreboardText.text = newText;
     }
 }
