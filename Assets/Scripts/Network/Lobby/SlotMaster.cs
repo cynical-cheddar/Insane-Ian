38141c1b720415e9f0f1943fdcf55228f4d87c05
Matt <matthew.crees@ntlworld.com>
2021-02-17 11:28:43 +0000

    Teams added and removed with buttons

--- Assets/Scripts/Network/Lobby/SlotMaster.cs
+++ Assets/Scripts/Network/Lobby/SlotMaster.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class SlotMaster : MonoBehaviour
 {
 
-    GamestateTracker gamestateTracker;
-    public LobbyButtonScript lbs;
-
     // Start is called before the first frame update
     void Start()
     {
-        gamestateTracker = FindObjectOfType<GamestateTracker>();
-        gamestateTracker.schema.teamsList.Add(new GamestateTracker.TeamDetails(lbs.teamId));
+
     }
 
     // Update is called once per frame
