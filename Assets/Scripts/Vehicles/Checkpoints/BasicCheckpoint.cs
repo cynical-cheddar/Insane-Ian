49318977be12a873c84aa0ed5d619a6f2072fc1f
Alex <x>
2021-03-17 11:43:04 +0000

    Much better for network now

--- Assets/Scripts/Vehicles/Checkpoints/BasicCheckpoint.cs
+++ Assets/Scripts/Vehicles/Checkpoints/BasicCheckpoint.cs
@@ -3,39 +3,34 @@ using UnityEngine;
 
 public class BasicCheckpoint : MonoBehaviour {
     public List<Transform> locations;
-    public Vector3 init() {
-        if (locations.Count < 1) {
-            Debug.LogError("Less than one checkpoint");
+    public int checkpointCount = 50;
+    private List<Transform> checkpointList = new List<Transform>();
+    private int prev, next;
+    private int current;
+
+    void Start() {
+        if (locations.Count < 2) {
+            Debug.LogError("Less than two checkpoints");
         } else {
-            int count = locations.Count;
-            int last = count - 1;
-            for (int i = 0; i < last; ++i) {
-                int r = Random.Range(i, count);
-                Transform tmp = locations[i];
-                locations[i] = locations[r];
-                locations[r] = tmp;
+            prev = Random.Range(0, locations.Count - 1);            
+            checkpointList.Add(locations[prev]);
+            next = prev;
+            for (int i = 0; i < checkpointCount - 1; ++i) {
+                while (next == prev) {
+                    next = Random.Range(0, locations.Count);
+                }
+                checkpointList.Add(locations[next]);
+                Debug.Log(locations[next]);
+                prev = next;
             }
         }
-        return locations[0].position;
+        current = 0;
     }
 
     public Vector3 nextCheckpoint(Vector3 t) {
-        int count = 0;
-        int i = -1;
-        List<Transform> newlist = new List<Transform>();
-
-        //find the index of the current position
-        foreach (Transform transform in locations) {
-            if (transform.position == t) i = count;
-            else newlist.Add(transform);
-            count += 1;
-        }
-
-        //if not found panic, return new random position
-        if (i == -1) {
-            Debug.LogError("Location not found");
-        }
-        return newlist[Random.Range(0, count - 2)].position;
+        current++;
+        if (current >= checkpointList.Count) current = 0;
+        return checkpointList[current].position;
 
 
     }
