ef8787de8b2cd7d1a0ca8d04a99e429fe274968a
Matt <matthew.crees@ntlworld.com>
2021-03-17 13:27:14 +0000

    Scoreboard always shows all teams, your team is larger

--- Assets/Scripts/UI/Scoring/TeamPanelBehaviour.cs
+++ Assets/Scripts/UI/Scoring/TeamPanelBehaviour.cs
@@ -11,4 +11,41 @@ public class TeamPanelBehaviour : MonoBehaviour
     public Image Position;
     public Image PositionShadow;
     public Image Glow;
+    public int id;
+    public GameObject parentPanel;
+    public bool isFirstPanel = false;
+    public bool isCurrentTeam = false;
+
+    Vector3 initialPosition;
+    public RectTransform rectTransform;
+
+    private void Start() {
+        initialPosition = transform.localPosition;
+        UpdateTransform(false);
+    }
+
+    public void UpdateTransform(bool isCurrentTeam) {
+        this.isCurrentTeam = isCurrentTeam;
+        if (isFirstPanel) {
+            if (isCurrentTeam) {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                transform.localPosition = initialPosition;
+            } else {
+                transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                transform.localPosition = initialPosition + new Vector3(70f, 20f, 0f);
+            }
+        } else {
+            if (isCurrentTeam) {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                transform.localPosition = parentPanel.transform.localPosition + new Vector3(-70f, -120f, 0f);
+            } else if () {
+                transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                transform.localPosition = parentPanel.transform.localPosition + new Vector3(70f, -120f, 0f);
+            } else {
+                transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                transform.localPosition = parentPanel.transform.localPosition + new Vector3(0f, -110f, 0f);
+            }
+        }
+    }
+
 }
