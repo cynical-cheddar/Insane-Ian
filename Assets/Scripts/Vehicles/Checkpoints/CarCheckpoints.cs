eb783d199856a8171305b7f4aaef838fdc20f4fd
Alex <x>
2021-03-17 15:13:10 +0000

    Networked again

--- Assets/Scripts/Vehicles/Checkpoints/CarCheckpoints.cs
+++ Assets/Scripts/Vehicles/Checkpoints/CarCheckpoints.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gamestate;
+using Photon.Pun;
 
 public class CarCheckpoints : MonoBehaviour
 {
     private int checkpoints = 0;
     private BasicCheckpoint bc;
     public Vector3 checkpointPos;
+    private GamestateTracker gamestateTracker;
     // Start is called before the first frame update
     void Start()
     {
+        gamestateTracker = FindObjectOfType<GamestateTracker>();
         bc = (BasicCheckpoint)FindObjectOfType(typeof(BasicCheckpoint));
         if (!bc) Debug.LogWarning("No BasicCheckpoint object could be found (this is fine in menus but BAD in game");
     }
@@ -18,8 +22,11 @@ public class CarCheckpoints : MonoBehaviour
         if (other.CompareTag("Checkpoint")) {
             checkpoints++;
             checkpointPos = bc.NextCheckpoint(checkpointPos);
-            Debug.Log(checkpoints);
             bc.gameObject.transform.position = checkpointPos;
+            TeamEntry team = gamestateTracker.teams.Get((short)gamestateTracker.players.Get((short)PhotonNetwork.LocalPlayer.ActorNumber).teamId);
+            team.checkpoint = (short)checkpoints;
+            team.Increment();
+            Debug.Log(team.checkpoint);
         }
     }
 }
