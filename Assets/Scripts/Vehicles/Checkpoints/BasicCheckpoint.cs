01313962abb50bd03a4c550dc44e828b78b93693
Alex <x>
2021-03-17 12:02:23 +0000

    Fixed for sending ints not transforms

--- Assets/Scripts/Vehicles/Checkpoints/BasicCheckpoint.cs
+++ Assets/Scripts/Vehicles/Checkpoints/BasicCheckpoint.cs
@@ -5,26 +5,43 @@ public class BasicCheckpoint : MonoBehaviour {
     public List<Transform> locations;
     public int checkpointCount = 50;
     private List<Transform> checkpointList = new List<Transform>();
-    private int prev, next;
+    private List<int> indexList = new List<int>();
+    private int prev1, prev2, next;
     private int current;
 
     void Start() {
-        if (locations.Count < 2) {
-            Debug.LogError("Less than two checkpoints");
-        } else {
-            prev = Random.Range(0, locations.Count - 1);            
-            checkpointList.Add(locations[prev]);
-            next = prev;
-            for (int i = 0; i < checkpointCount - 1; ++i) {
-                while (next == prev) {
-                    next = Random.Range(0, locations.Count);
+        bool host = true;
+
+        if (host) { //if you are the host, generate checkpoints
+            if (locations.Count < 3) {
+                Debug.LogError("Less than three checkpoints");
+            } else {
+                prev1 = Random.Range(0, locations.Count - 1);            
+                indexList.Add(prev1); //add one checkpoint
+                next = prev1;
+                prev2 = prev1;
+                for (int i = 0; i < checkpointCount - 1; ++i) { // add sequential checkpoints
+                    while (next == prev1 || next == prev2) { //next checkpoint cannot be either of the previous two
+                        next = Random.Range(0, locations.Count);
+                    }
+                    indexList.Add(next);
+                    Debug.Log(next);
+                    prev2 = prev1;
+                    prev1 = next;
                 }
-                checkpointList.Add(locations[next]);
-                Debug.Log(locations[next]);
-                prev = next;
             }
+        } else { // if not host, get checkpoint index list
+            indexList = getListFromHost();
         }
+        foreach (int i in indexList){ // turn index list into location list
+            checkpointList.Add(locations[i]);
+        }
+
         current = 0;
+    }
+
+    private List<int> getListFromHost() {
+        throw new System.NotImplementedException();
     }
 
     public Vector3 nextCheckpoint(Vector3 t) {
