9a5d1bf5a384dd8f7356dee6645ab9a4782cc034
Matt <matthew.crees@ntlworld.com>
2021-03-18 13:39:15 +0000

    Health bar

--- Assets/Scripts/UI/DamageIndicatorBehaviour.cs
+++ Assets/Scripts/UI/DamageIndicatorBehaviour.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DamageIndicatorBehaviour : MonoBehaviour
 {
     HealthBehaviour healthBehaviour;
-    public Text damageText;
+    public TextMeshProUGUI damageText;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,6 +15,8 @@ public class DamageIndicatorBehaviour : MonoBehaviour
         damageText.text = healthBehaviour.damageTaken.ToString();
         if (healthBehaviour.damageTaken > 0) {
             damageText.color = new Color(0, 204, 0);
+        } else {
+            damageText.color = new Color(204, 0, 0);
         }
         Invoke(nameof(Die), 0.5f);
     }
