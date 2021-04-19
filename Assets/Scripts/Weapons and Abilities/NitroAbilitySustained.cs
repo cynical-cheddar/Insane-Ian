2ef7c07cc1f62d6e923ee1d18b98899fbc3bb906
Alex <x>
2021-04-19 15:25:43 +0100

    zBike updated

--- Assets/Scripts/Weapons and Abilities/NitroAbilitySustained.cs
+++ Assets/Scripts/Weapons and Abilities/NitroAbilitySustained.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class NitroAbilitySustained : DriverAbility
-{
+public class NitroAbilitySustained : DriverAbility {
     // Start is called before the first frame update
     public float activationVelocityChange = 50f;
     [Header("Force Parameters")]
@@ -20,7 +17,7 @@ public class NitroAbilitySustained : DriverAbility
     public float steerRateLerp = 0.1f;
     [Range(0, 1)]
     public float baseExtremiumSlip = 0.3f;
-    [Range(0,20000)]
+    [Range(0, 20000)]
     public float antiRollStiffness = 5000;
 
     private float oldMaxSteerAngle;
@@ -32,20 +29,19 @@ public class NitroAbilitySustained : DriverAbility
     private float oldAntiRollStiffness;
 
     private InterfaceCarDrive4W interfaceCarDrive4W;
-    
-    public override void SetupAbility()
-    {
+
+    public override void SetupAbility() {
         Debug.LogWarning("Nitro Ability Sustained has not been ported to the new PhysX system");
         base.SetupAbility();
         interfaceCarDrive4W = GetComponentInParent<InterfaceCarDrive4W>();
-        
+
         oldMaxSteerAngle = interfaceCarDrive4W.maxSteerAngle;
         oldMotorTorque = interfaceCarDrive4W.motorTorque;
         oldBrakeTorque = interfaceCarDrive4W.brakeTorque;
         oldBrakeForce = interfaceCarDrive4W.brakeForce;
         oldbaseExtremiumSlip = interfaceCarDrive4W.baseExtremiumSlip;
         oldAntiRollStiffness = interfaceCarDrive4W.antiRollStiffness;
-        
+
         interfaceCarDrive4W.maxSteerAngle = maxSteerAngle;
         interfaceCarDrive4W.motorTorque = motorTorque;
         interfaceCarDrive4W.brakeTorque = brakeTorque;
@@ -54,18 +50,15 @@ public class NitroAbilitySustained : DriverAbility
         interfaceCarDrive4W.antiRollStiffness = antiRollStiffness;
     }
 
-    public override void ResetAbility()
-    {
+    public override void ResetAbility() {
         base.ResetAbility();
         DeactivateAbility();
 
     }
 
 
-    public override void ActivateAbility()
-    {
-        if (!isSetup)
-        {
+    public override void ActivateAbility() {
+        if (!isSetup) {
             SetupAbility();
         }
 
@@ -77,7 +70,7 @@ public class NitroAbilitySustained : DriverAbility
         oldBrakeForce = interfaceCarDrive4W.brakeForce;
         oldbaseExtremiumSlip = interfaceCarDrive4W.baseExtremiumSlip;
         oldAntiRollStiffness = interfaceCarDrive4W.antiRollStiffness;
-        
+
         interfaceCarDrive4W.maxSteerAngle = maxSteerAngle;
         interfaceCarDrive4W.motorTorque = motorTorque;
         interfaceCarDrive4W.brakeTorque = brakeTorque;
@@ -87,32 +80,29 @@ public class NitroAbilitySustained : DriverAbility
 
         Transform me = interfaceCarDrive4W.transform;
         me.GetComponent<Rigidbody>().AddForce(me.forward * activationVelocityChange, ForceMode.VelocityChange);
-        
+
         abilityPhotonView.RPC(nameof(ActivationEffects_RPC), RpcTarget.All);
     }
-    public override void DeactivateAbility()
-    {
+    public override void DeactivateAbility() {
         abilityActivated = false;
-        
+
         interfaceCarDrive4W.maxSteerAngle = oldMaxSteerAngle;
         interfaceCarDrive4W.motorTorque = oldMotorTorque;
         interfaceCarDrive4W.brakeTorque = oldBrakeTorque;
         interfaceCarDrive4W.brakeForce = oldBrakeForce;
         interfaceCarDrive4W.baseExtremiumSlip = oldbaseExtremiumSlip;
         interfaceCarDrive4W.antiRollStiffness = oldAntiRollStiffness;
-       
+
     }
     // sustained ability set in manager - called every while active
-    public override void Fire()
-    {
-        if (CanUseAbility())
-        {
+    public override void Fire() {
+        if (CanUseAbility()) {
             currentCooldown = cooldown;
             timeSinceLastFire = 0;
             UseCharge(chargeUsedPerFire);
-            
+
         }
     }
 
-    
+
 }
