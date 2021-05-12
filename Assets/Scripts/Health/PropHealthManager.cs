95bf766c69cb527fe054638c326c86cd03c7876b
Matt <matthew.crees@ntlworld.com>
2021-05-12 16:57:16 +0100

    fixed broken crates

--- Assets/Scripts/Health/PropHealthManager.cs
+++ Assets/Scripts/Health/PropHealthManager.cs
@@ -9,19 +9,21 @@ public class PropHealthManager : CollidableHealthManager
     public GameObject wreckPrefab;
     
     protected override void Die() {
-        //Debug.Log("Dead prop");
+        Debug.Log("Dead prop");
         health = 0;
         isDead = true;
         myPhotonView.RPC(nameof(PlayDeathEffects_RPC), RpcTarget.AllViaServer);
-        PhotonNetwork.Destroy(gameObject);
     }
     
     [PunRPC]
     protected override void PlayDeathEffects_RPC()
     {
+        Debug.Log("Playing death effects");
         if (wreckPrefab != null) {
+            Debug.Log("Found wreck prafab");
             Instantiate(wreckPrefab, transform.position, transform.rotation);  
         }
-        
+        Destroy(gameObject);
+
     }
 }
