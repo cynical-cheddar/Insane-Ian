fe5451bd4388d9ba1c82af0e8e3423ff90ef3a83
Amber <almrothera@gmail.com>
2021-05-24 12:30:21 +0100

    Amber Comments

--- Assets/Scripts/Health/PropHealthManager.cs
+++ Assets/Scripts/Health/PropHealthManager.cs
@@ -1,3 +1,5 @@
+//Prop health manager manages props which have health and then break into anothyer prefab upon death.
+
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
@@ -7,19 +9,19 @@ public class PropHealthManager : MonoBehaviour, ICollisionEnterEvent
 {
 
     public bool requiresData { get { return true; } }
-    public GameObject wreckPrefab;
+    public GameObject wreckPrefab; //The prefab an object will break into
 
    // public bool requiresData { get { return true; } }
 
     public void CollisionEnter() {}
 
-    public void CollisionEnter(PhysXCollision col){
+    public void CollisionEnter(PhysXCollision col){ //When hit at a certain velocity, the object will call Die()
         if (col.rigidBody != null && col.rigidBody.velocity.magnitude > 3) {
             Die();
         }
     }
 
-    protected void Die() {
+    protected void Die() { //Is called when an object breaks, insstantiates new prefab and destroys old gameObject
         if(wreckPrefab!=null){
             Instantiate(wreckPrefab, transform.position, transform.rotation);
         }
